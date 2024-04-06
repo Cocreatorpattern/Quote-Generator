93e8d069f5e62eb15c3b2200cb7ba56af5b578c4
Cocreatorpattern <136176709+Cocreatorpattern@users.noreply.github.com>
2024-04-06 16:43:30 +0100

    changed function to quote

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-function displayJoke(response) {
-   new Typewriter("#poem", {
+function displayQuote(response) {
+   new Typewriter("#quote", {
      strings: response.data.answer,
      autoStart: true,
      delay: 1,
@@ -7,22 +7,22 @@ function displayJoke(response) {
    });
 }
 
-function generateJoke(event) {
+function generateQuote(event) {
    event.preventDefault();
 
    let instructionsInput = document.querySelector("#user-instructions");
    let apiKey = "2046c535afeb092fo82f1d306d8a2b2t";
    let context =
-     "You are a comediant. You mission is to tell really good jokes";
-   let prompt = `User instructions: Generate a joke about ${instructionsInput.value}`;
+     "You are a inspirator. You mission is to tell motivational quotes";
+   let prompt = `User instructions: Generate a quote about ${instructionsInput.value}`;
    let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-   let poemElement = document.querySelector("#poem");
-   poemElement.classList.remove("hidden");
-   poemElement.innerHTML = `<div class="generating">⏳ Generating a joke about ${instructionsInput.value}</div>`;
+   let quoteElement = document.querySelector("#quote");
+   quoteElement.classList.remove("hidden");
+   quoteElement.innerHTML = `<div class="generating">⏳ Generating a quote about ${instructionsInput.value}</div>`;
 
-   axios.get(apiURL).then(displayJoke);
+   axios.get(apiURL).then(displayQuote);
 }
 
-let poemFormElement = document.querySelector("#poem-generator-form");
-poemFormElement.addEventListener("submit", generateJoke);
+let quoteFormElement = document.querySelector("#quote-generator-form");
+quoteFormElement.addEventListener("submit", generateQuote);
